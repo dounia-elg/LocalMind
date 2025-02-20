d01d97ec9f1d8f4ad8ee8dccb2dc54b55d01faea
dounia-elg <dounia.elg20@gmail.com>
2025-02-20 14:51:32 +0100

    Implémenter FavoriteController

--- LocalMind/app/Http/Controllers/FavoriteController.php
+++ LocalMind/app/Http/Controllers/FavoriteController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class FavoriteController extends Controller
 {
-    //
+    public function toggle(Question $question) {
+        auth()->user()->favorites()->toggle($question->id);
+        return back();
+    }
 }
+
+
+
