5d708839b3232b71299230be15f0b53a2c1c9b4b
dounia-elg <dounia.elg20@gmail.com>
2025-02-20 14:41:02 +0100

    Créer Answer model

--- LocalMind/app/Models/Answer.php
+++ LocalMind/app/Models/Answer.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['user_id', 'question_id', 'content'];
+
+    public function user() { return $this->belongsTo(User::class); }
+    public function question() { return $this->belongsTo(Question::class); }
 }
+
+
