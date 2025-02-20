326b4281ac1e622ea115ee01b87f066ef8ccca62
dounia-elg <dounia.elg20@gmail.com>
2025-02-20 14:40:16 +0100

    Créer Question model

--- LocalMind/app/Models/Question.php
+++ LocalMind/app/Models/Question.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['user_id', 'title', 'content', 'location'];
+    
+    public function user() { return $this->belongsTo(User::class); }
+    public function answers() { return $this->hasMany(Answer::class); }
+    public function favorites() { return $this->hasMany(Favorite::class); }
 }
+
+
+
