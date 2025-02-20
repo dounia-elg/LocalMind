ec39bb86e09de1a92b5a0928bbb2b193ad152020
dounia-elg <dounia.elg20@gmail.com>
2025-02-20 12:29:25 +0100

    Configuration des Routes

--- LocalMind/routes/web.php
+++ LocalMind/routes/web.php
@@ -15,6 +15,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('questions', QuestionController::class);
+    Route::resource('answers', AnswerController::class)->only(['store']);
+    Route::post('favorites/{question}', [FavoriteController::class, 'toggle'])->name('favorites.toggle');
 });
 
 require __DIR__.'/auth.php';
+
+
+
