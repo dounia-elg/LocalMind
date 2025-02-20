faa46b13a34764d92c1be7e050b854b5758377ad
dounia-elg <dounia.elg20@gmail.com>
2025-02-20 14:42:15 +0100

    Créer Favorite model

--- LocalMind/app/Models/Favorite.php
+++ LocalMind/app/Models/Favorite.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Favorite extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['user_id', 'question_id'];
+
+    public function user() { return $this->belongsTo(User::class); }
+    public function question() { return $this->belongsTo(Question::class); }
 }
+
+
